3ce9e1afec127d110935fec23a74f493fc1ee75d
k1mlka <k1mlka@outlook.com>
2025-05-15 12:22:45 +0800

    add package

--- AvaloniaCrossEssential/ViewModels/MainViewModel.cs
+++ AvaloniaCrossEssential/ViewModels/MainViewModel.cs
@@ -1,7 +1,17 @@
 ﻿
+using Microsoft.Maui.ApplicationModel;
+using ReactiveUI;
+using System.Reactive;
+
 namespace AvaloniaCrossEssential.ViewModels;
 
 public class MainViewModel : ViewModelBase
 {
     public string Greeting { get; } = "Welcome to Avalonia!";
+
+    public ReactiveCommand<Unit, Unit> TestCommand { get; } = ReactiveCommand.CreateFromTask(async () =>
+    {
+        // Simulate a long-running task
+        await Browser.OpenAsync("https://www.google.com");
+    });
 }
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # AvaloniaCrossEssential
+
+commit 有相较于模板项目的变更记录
+
+1. 对 Microsoft.Maui.Essentials 的依赖似乎必须 net9.0
